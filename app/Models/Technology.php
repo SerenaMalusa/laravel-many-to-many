92bd9514b166cf07fdc8f3aa29f31a0061d6a5dc
SerenaMalusa <sere.malu@gmail.com>
2024-04-08 15:22:57 +0200

    feat: complete Technology model and update Projects model

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Technology extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
